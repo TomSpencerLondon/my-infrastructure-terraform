3319cc6379f3c8e0750dcfdd37d4901d8d7d6c72
tom <tomspencerlondon@gmail.com>
2023-05-18 13:24:30 +0100

    Edit region

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "my-aws-tf-state-bucket-tom"
     key = "main"
-    region = "us-east-1"
+    region = "eu-west-2"
     dynamodb_table = "my-dynamo-db-table"
   }
 }
