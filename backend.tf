55f655ca3e5ee6698decc7b8c23c43df69977cf7
tom <tomspencerlondon@gmail.com>
2023-05-18 13:32:34 +0100

    Edit bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket-tom"
+    bucket = "my-aws-tf-state-bucket-toms"
     key = "main"
     region = "eu-west-3"
     dynamodb_table = "my-dynamo-db-table"
